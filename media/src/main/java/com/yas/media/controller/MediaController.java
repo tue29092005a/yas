c85111df87a564c7bc98d1727620cfcf89740aea
23120398 <vothanhminhtuea@gmail.com>
2026-04-30 17:06:47 +0700

    test ci again

--- media/src/main/java/com/yas/media/controller/MediaController.java
+++ media/src/main/java/com/yas/media/controller/MediaController.java
@@ -29,7 +29,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-#hello test
+// hello test
 @Validated
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
