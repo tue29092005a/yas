01224649e996bdae2b49ee4931ad852ae3a43a50
23120398 <vothanhminhtuea@gmail.com>
2026-05-01 22:10:31 +0700

    test ci

--- media/src/test/java/com/yas/media/controller/MediaControllerTest.java
+++ media/src/test/java/com/yas/media/controller/MediaControllerTest.java
@@ -33,7 +33,7 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-
+// ts
 @WebMvcTest(excludeAutoConfiguration = OAuth2ResourceServerAutoConfiguration.class)
 @ContextConfiguration(classes = {
     MediaController.class,
