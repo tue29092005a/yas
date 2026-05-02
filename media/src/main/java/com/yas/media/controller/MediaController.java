17a8f81de6d3d18d60b290f3cea7f4603b837734
23120398 <vothanhminhtuea@gmail.com>
2026-05-02 16:04:43 +0700

    update to shad versions and test ci

--- media/src/main/java/com/yas/media/controller/MediaController.java
+++ media/src/main/java/com/yas/media/controller/MediaController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 // Trigger CI for media module sfwef
-// test 
+// test 2
 @Validated
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
