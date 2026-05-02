c06ba199e01c48758f2ff58bdd6cc0c2effb2da1
23120398 <vothanhminhtuea@gmail.com>
2026-05-02 13:41:11 +0700

    test ci

--- media/src/main/java/com/yas/media/controller/MediaController.java
+++ media/src/main/java/com/yas/media/controller/MediaController.java
@@ -29,7 +29,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-// Trigger CI for media module
+// Trigger CI for media module sfwef
 @Validated
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
